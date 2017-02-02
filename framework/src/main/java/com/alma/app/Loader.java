31e0f5e34db998ab8988f334d7bbc38ae988a52c
Alexis <abonnin44@gmail.com>
2017-02-02 19:21:18 +0100

    Add some class for the framework

---
+++ framework/src/main/java/com/alma/app/Loader.java
@@ -0,0 +1,15 @@
+package com.alma.app;
+
+public class Loader {
+
+	public List<Description>	
+	
+	public void loadMainApp(String configFile){
+		
+	}
+	
+	public Object loadExtension(String description){
+		
+		return null;
+	}
+}
