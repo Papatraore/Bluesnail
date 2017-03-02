c6945cc9345d97ec7589ac8cd6c1536ac213ec8c
Alexis <abonnin44@gmail.com>
2017-03-02 17:58:15 +0100

    Changement framework

---
+++ framework/src/main/java/com/alma/app/App.java
@@ -0,0 +1,14 @@
+package com.alma.app;
+
+/**
+ * Run the framework and load an application give by user.
+ * 
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Starting Bluesnail..." );
+        
+    }
+}
