004c4885b30f0ba1095eac7391d2b19900cc2a39
Alexis <abonnin44@gmail.com>
2017-03-16 16:54:26 +0100

    ajout du plugin descriptor git status!

--- framework/src/main/java/com/alma/platform/PluginDescriptor.java
+++ framework/src/main/java/com/alma/platform/PluginDescriptor.java
@@ -2,4 +2,40 @@ package com.alma.platform;
 
 public class PluginDescriptor {
 
+	private String name;
+	private String className;
+	private String interfaceName;
+	private String directoryPath;
+	private boolean autorun;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public String getInterfaceName() {
+		return interfaceName;
+	}
+	public void setInterfaceName(String interfaceName) {
+		this.interfaceName = interfaceName;
+	}
+	public String getDirectoryPath() {
+		return directoryPath;
+	}
+	public void setDirectoryPath(String directoryPath) {
+		this.directoryPath = directoryPath;
+	}
+	public boolean isAutorun() {
+		return autorun;
+	}
+	public void setAutorun(boolean autorun) {
+		this.autorun = autorun;
+	}	
 }
